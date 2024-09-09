bacccdbd9d76ce738ca803c11dc21eab2155603d
Ferymad <94054581+Ferymad@users.noreply.github.com>
2024-09-09 19:09:10 +0100

    Initial commit for v01 branch

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# ... (rest of your app code)
+
+# At the bottom of your main app file or in your footer component
+st.markdown("""
+---
+### Contact the Developer
+* Telegram: [@your_telegram_username](https://t.me/asosyalarsivdev)
+""")
